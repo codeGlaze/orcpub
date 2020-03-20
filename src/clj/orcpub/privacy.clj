7d0748de9e26092dfcebf4e192db29f4491ef839
DatDamnZotz <john@lissproductions.com>
2020-03-20 15:58:28 -0600

    Update privacy.clj

--- src/clj/orcpub/privacy.clj
+++ src/clj/orcpub/privacy.clj
@@ -20,7 +20,14 @@
    [{:title "Thank you for using Dungeon Master's Vault!"
      :font-size 32
      :paragraphs
-     ["We wrote this policy to help you understand what information we collect, how we use it, and what choices you have. Because we're an internet company, some of the concepts below are a little technical, but we've tried our best to explain things in a simple and clear way. We welcome your questions and comments on this policy."]}
+     ["We wrote this policy to help you understand what information we collect, how we use it, and what choices you have. Because we're an internet company, some of the concepts below are a little technical, but we've tried our best to explain things in a simple and clear way. We welcome your questions and comments on this policy."
+     "At Dungeon Master's Vault, accessible from https://www.dungeonmastersvault.com, https://faq.dungeonmastersvault.com https://lists.dungeonmastersvault.com or any host under the domain name *.dungeonmastersvault.com"
+     "This Privacy Policy document contains types of information that is collected and recorded by Dungeon Master's Vault and how we use it. If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us."
+     "This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Dungeon Master's Vault. This policy is not applicable to any information collected offline or via channels other than this website."]}
+    {:title "Consent"
+     :font-size 32
+     :paragraphs
+     ["By using our website, you hereby consent to our Privacy Policy and agree to its terms."]}
     {:title "How We Collect Your Information"
      :font-size 32
      :subsections
@@ -34,7 +41,7 @@
        :paragraphs
        ["These days, whenever you use a website, mobile application, or other internet service, there's certain information that almost always gets created and recorded automatically. The same is true when you use our products. Here are some of the types of information we collect:"
         "Log data. When you use Dungeon Master's Vault, our servers may automatically record information (\"log data\"), including information that your browser sends whenever you visit a website or your mobile app sends when you're using it. This log data may include your Internet Protocol address, the address of the web pages you visited that had Dungeon Master's Vault features, browser type and settings, the date and time of your request, how you used Dungeon Master's Vault, and cookie data."
-        "Cookie data. Depending on how you're accessing our products, we may use \"cookies\" (small text files sent by your computer each time you visit our website, unique to your Dungeon Master's Vault account or your browser) or similar technologies to record log data. When we use cookies, we may use \"session\" cookies (that last until you close your browser) or \"persistent\" cookies (that last until you or your browser delete them). For example, we may use cookies to store your language preferences or other Dungeon Master's Vault settings so you don‘t have to set them up every time you visit Dungeon Master's Vault. Some of the cookies we use are associated with your Dungeon Master's Vault account (including personal information about you, such as the email address you gave us), and other cookies are not."
+        "Cookie data. Depending on how you're accessing our products, we may use \"cookies\" (small text files sent by your computer each time you visit our website, unique to your Dungeon Master's Vault account or your browser) or similar technologies to record log data. When we use cookies, we may use \"session\" cookies (that last until you close your browser) or \"persistent\" cookies (that last until you or your browser delete them). For example, we may use cookies to store your language preferences or other Dungeon Master's Vault settings so you don't have to set them up every time you visit Dungeon Master's Vault. Some of the cookies we use are associated with your Dungeon Master's Vault account (including personal information about you, such as the email address you gave us), and other cookies are not."
         "Device information. In addition to log data, we may also collect information about the device you're using Dungeon Master's Vault on, including what type of device it is, what operating system you're using, device settings, unique device identifiers, and crash data. Whether we collect some or all of this information often depends on what type of device you're using and its settings. For example, different types of information are available depending on whether you're using a Mac or a PC, or an iPhone or an Android phone. To learn more about what information your device makes available to us, please also check the policies of your device manufacturer or software provider."]}
       {:title "Our partners and advertisers may share information with us"
        :font-size 28
@@ -64,15 +71,37 @@
       "Online advertisers typically use third party companies to audit the delivery and performance of their ads on websites and apps. We also allow these companies to collect this information on Dungeon Master's Vault. To learn more, please see our Help Center."
       "We may employ third party companies or individuals to process personal information on our behalf based on our instructions and in compliance with this Privacy Policy. For example, we share credit card information with the payment companies we use to store your payment information. Or, we may share data with a security consultant to help us get better at identifying spam. In addition, some of the information we request may be collected by third party providers on our behalf."
       "If we believe that disclosure is reasonably necessary to comply with a law, regulation or legal request; to protect the safety, rights, or property of the public, any person, or Dungeon Master's Vault; or to detect, prevent, or otherwise address fraud, security or technical issues. We may share the information described in this Policy with our wholly-owned subsidiaries and affiliates. We may engage in a merger, acquisition, bankruptcy, dissolution, reorganization, or similar transaction or proceeding that involves the transfer of the information described in this Policy. We may also share aggregated or non-personally identifiable information with our partners, advertisers or others."]}
+    {:title "Advertising Partners Privacy Policies"
+     :font-size 32
+     :paragraphs
+     ["You may consult this list to find the Privacy Policy for each of the advertising partners of Dungeon Master's Vault.Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Dungeon Master's Vault, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.Note that Dungeon Master's Vault has no access to or control over these cookies that are used by third-party advertisers"]}
+    {:title "Third Party Privacy Policies"
+     :font-size 32
+     :paragraphs
+     ["Dungeon Master's Vault's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options. You may find a complete list of these Privacy Policies and their links here: Privacy Policy Links.You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites."]}
+    {:title "CCPA Privacy Rights (Do Not Sell My Personal Information)"
+     :font-size 32
+     :paragraphs
+     ["Under the CCPA, among other rights, California consumers have the right to:"
+      "Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers."
+      "Request that a business delete any personal data about the consumer that a business has collected."
+      "Request that a business that sells a consumer's personal data, not sell the consumer's personal data."
+      "If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us."]}
+    {:title "GDPR Data Protection Rights"
+     :font-size 32
+     :paragraphs
+     ["We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:"
+      "The right to access. You have the right to request copies of your personal data. "
+      "We may charge you a small fee for this service. The right to rectification. You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.The right to erasure. You have the right to request that we erase your personal data, under certain conditions.The right to restrict processing. You have the right to request that we restrict the processing of your personal data, under certain conditions.The right to object to processing. You have the right to object to our processing of your personal data, under certain conditions.The right to data portability. You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us."]}
     {:title "What choices do you have about your information?"
      :font-size 32
      :paragraphs
-     ["You may close your account at any time by emailing thDM@dungeonmastersvault.com. We will then inactivate your account and remove your content from Dungeon Master's Vault. We may retain archived copies of you information as required by law or for legitimate business purposes (including to help address fraud and spam). You may remove any content you create from Dungeon Master's Vault at any time, although we may retain archived copies of the information. You may also disable sharing of content you create at any time, whether publicly shared or privately shared with specific users."
+     ["You may close/delete your account at any time by emailing thDM@dungeonmastersvault.com. We will then inactivate your account and remove your content from Dungeon Master's Vault. We may retain archived copies of you information as required by law or for legitimate business purposes (including to help address fraud and spam). You may remove any content you create from Dungeon Master's Vault at any time, although we may retain archived copies of the information. You may also disable sharing of content you create at any time, whether publicly shared or privately shared with specific users."
       "Also, we support the Do Not Track browser setting."]}
-    {:title "Our policy on children's information"
+    {:title "Children's Information"
      :font-size 32
      :paragraphs
-     ["Dungeon Master's Vault is not directed to children under 13. If you learn that your minor child has provided us with personal information without your consent, please contact us."]}
+     ["Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. Dungeon Master's Vault does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records."]}
     {:title "How do we make changes to this policy?"
      :font-size 32
      :paragraphs
