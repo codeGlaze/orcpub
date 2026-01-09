b6f378621a2a03917a4f7d9fe6b37cac0c70b9f3
codeGlaze <github@codeglaze.com>
2026-01-09 05:18:30 +0000

    chore: remove start-datomic-local wrapper (renamed to start-datomic-auto)

--- scripts/start-datomic-local.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Wrapper for backward compatibility: delegate to start-datomic-auto.sh
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-exec "$SCRIPT_DIR/start-datomic-auto.sh" "$@"
