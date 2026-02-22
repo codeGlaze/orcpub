55e9f491f3b0f4fd5844388525c89f1e3cd0345a
codeGlaze <github@codeglaze.com>
2026-02-22 21:13:02 +0000

    fix: Alpine compatibility for Docker images
    - orcpub builder: use Alpine variant, install curl/unzip/bash explicitly
    - orcpub runner: add font packages for PDFBox PDF generation
    - datomic: add bash (required by transactor script)
    - BusyBox sed: reorder flags to -i before expression (start.sh + Dockerfile)

--- deploy/start.sh
+++ deploy/start.sh
@@ -10,18 +10,18 @@ if [ -z "$DATOMIC_PASSWORD" ]; then
   exit 1
 fi
 
-sed "/host=datomic/a alt-host=${ALT_HOST:-127.0.0.1}" -i  /datomic/transactor.properties
-sed "s/# storage-admin-password=/storage-admin-password=${ADMIN_PASSWORD}/" -i /datomic/transactor.properties
-sed "s/# storage-datomic-password=/storage-datomic-password=${DATOMIC_PASSWORD}/" -i /datomic/transactor.properties
+sed -i "/host=datomic/a alt-host=${ALT_HOST:-127.0.0.1}" /datomic/transactor.properties
+sed -i "s/# storage-admin-password=/storage-admin-password=${ADMIN_PASSWORD}/" /datomic/transactor.properties
+sed -i "s/# storage-datomic-password=/storage-datomic-password=${DATOMIC_PASSWORD}/" /datomic/transactor.properties
 
 if [ -n "$ADMIN_PASSWORD_OLD" ]; then
-  sed "s/# old-storage-admin-password=/old-storage-admin-password=$ADMIN_PASSWORD_OLD/" -i /datomic/transactor.properties
+  sed -i "s/# old-storage-admin-password=/old-storage-admin-password=$ADMIN_PASSWORD_OLD/" /datomic/transactor.properties
 fi
 
 if [ -n "$DATOMIC_PASSWORD_OLD" ]; then
-  sed "s/# old-storage-datomic-password=/old-storage-datomic-password=$DATOMIC_PASSWORD_OLD/" -i /datomic/transactor.properties
+  sed -i "s/# old-storage-datomic-password=/old-storage-datomic-password=$DATOMIC_PASSWORD_OLD/" /datomic/transactor.properties
 fi
 
-sed "s/# encrypt-channel=true/encrypt-channel=${ENCRYPT_CHANNEL:-true}/" -i /datomic/transactor.properties
+sed -i "s/# encrypt-channel=true/encrypt-channel=${ENCRYPT_CHANNEL:-true}/" /datomic/transactor.properties
 
 /datomic/bin/transactor /datomic/transactor.properties
