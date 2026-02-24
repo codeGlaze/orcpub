9e1d0ffb3668446534d5e0f1ab4a2a4b16806ac6
codeGlaze <github@codeglaze.com>
2026-02-24 19:24:03 +0000

    fix: remove debug subscribe wrapper that breaks advanced compilation
    The monkey-patch on re-frame.core/subscribe used a variadic fn
    (fn [query-v & args] ...) which doesn't implement IFn's 1-arity
    .h method. In advanced compilation, all (subscribe [...]) call sites
    compile to Y.h(vec) using 1-arity protocol dispatch, causing
    "Y.h is not a function" TypeError on every render.

--- web/cljs/orcpub/core.cljs
+++ web/cljs/orcpub/core.cljs
@@ -12,7 +12,7 @@
             [clojure.string :as s]
             [re-frame.core :refer [dispatch dispatch-sync subscribe]]
             [reagent.core :as r]
-            [reagent.ratom :as ratom]
+
             [reagent.dom.client :as rdc]
             [goog.events])
   (:import
@@ -31,17 +31,6 @@
 ;; Must be called here (not self-initializing) so equipment-subs has loaded.
 (autosave-fx/init-template-cache!)
 
-;; DEBUG: trace subscribe calls outside reactive context.
-;; Prints a full stack trace so we can find the call site.
-;; REMOVE after fixing the warnings.
-(let [original-subscribe re-frame.core/subscribe]
-  (set! re-frame.core/subscribe
-        (fn [query-v & args]
-          (when-not (ratom/reactive?)
-            (js/console.warn "[TRACE] subscribe outside reactive context:"
-                             (pr-str query-v))
-            (js/console.trace))
-          (apply original-subscribe query-v args))))
 
 (def pages
   {nil views-2/splash-page
